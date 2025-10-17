39ed4b2dab62985971796ba213a02ebdef379f24
moralesk <kmorales529@gmail.com>
2025-10-17 16:58:35 -0400

    Initial Commit

---
+++ SavewiseMinimalRepro/SavewiseMinimalReproApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavewiseMinimalReproApp.swift
+//  SavewiseMinimalRepro
+//
+//  Created by Kelly Morales on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SavewiseMinimalReproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
