9a14a4573d0d9a640983a76499b42a89de7d1c0e
moralesk <kmorales529@gmail.com>
2025-10-17 18:35:07 -0400

    Integrate Clerk SDK Sign in with Apple functionality.

--- SavewiseMinimalRepro/ContentView.swift
+++ SavewiseMinimalRepro/ContentView.swift
@@ -5,17 +5,30 @@
 //  Created by Kelly Morales on 10/17/25.
 //
 
+import Clerk
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.clerk)
+    private var clerk
+
+    @State
+    private var isAuthPresented = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if clerk.user != nil {
+                UserButton()
+                    .frame(width: 36, height: 36)
+            } else {
+                Button("Sign in") {
+                    isAuthPresented = true
+                }
+            }
         }
-        .padding()
+        .sheet(isPresented: $isAuthPresented) {
+            AuthView()
+        }
     }
 }
 
--- SavewiseMinimalRepro/SavewiseMinimalReproApp.swift
+++ SavewiseMinimalRepro/SavewiseMinimalReproApp.swift
@@ -5,13 +5,21 @@
 //  Created by Kelly Morales on 10/17/25.
 //
 
+import Clerk
 import SwiftUI
 
 @main
 struct SavewiseMinimalReproApp: App {
+    @State private var clerk = Clerk.shared
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.clerk, clerk)
+                .task {
+                    clerk.configure(publishableKey: "pk_test_ZXBpYy1tb2xsdXNrLTExLmNsZXJrLmFjY291bnRzLmRldiQ")
+                    try? await clerk.load()
+                }
         }
     }
 }
